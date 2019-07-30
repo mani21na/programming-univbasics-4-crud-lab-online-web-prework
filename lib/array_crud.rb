383be6ad099a012ec69ed25b098ccd5d02f78de4
Hanna Lee <mani21na@gmail.com>
2019-07-30 21:48:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [] 
   array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["a","b","c","d"]
   array.unshift(element)
 end
 
